bb4a6f338c3de3f3410b28269e4b19eb037ce627
VVelos0 <vitor-velososl@hotmail.com>
2024-12-05 14:49:06 -0300

    Delete LeituraJSON.py

--- LeituraJSON.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def retornar_json():
-    with open ("TestePartidaJSON.json",  encoding="utf - 8") as json_normal:
-
-        json_manipulavel = json.load("json_normal")
-        return json_manipulavel
