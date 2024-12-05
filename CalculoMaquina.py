5516ee95ec6c8177a70539a7f990dce48d909e7c
VVelos0 <vitor-velososl@hotmail.com>
2024-12-05 14:59:13 -0300

    Delete CalculoMaquina.py

--- CalculoMaquina.py
+++
@@ -1,9 +0,0 @@
-def maior_de_idade(idade):
-    if idade >= 18:
-        print('Maior de 18')
-    else:
-        print('Menor de idade')
-
-
-idade = int(input("Digite sua idade: "))
-maior_de_idade(idade)
